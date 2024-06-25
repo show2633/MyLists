0f08fd9b233942a4aa77280662cad7cbafd77a9c
First Snow <show2633@naver.com>
2024-06-25 21:41:21 +0900

    Initial Commit

---
+++ MyLists/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyLists
+//
+//  Created by Hyunwoo Shin on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("MY LISTS")
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyLists/MyListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyListsApp.swift
+//  MyLists
+//
+//  Created by Hyunwoo Shin on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MyListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
